53cb706e417e08072a5c82972e233efedb2a7de2
Lorenzo Capetta <lorenzo.capetta@studenti.unipd.it>
2023-04-29 12:28:24 +0200

    introduce login personalization

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -36,6 +36,83 @@ class LoginPage extends StatelessWidget {
 
     return FlutterLogin(
       title: 'Welcome!',
+
+
+      //logo: AssetImage('assets/images/ecorp-lightblue.png'),
+
+      theme: LoginTheme(
+        primaryColor: Colors.green,
+        accentColor: Colors.black,
+        errorColor: Colors.deepOrange,
+        titleStyle: const TextStyle(
+          color: Colors.white,
+          fontFamily: 'Quicksand',
+          letterSpacing: 1,
+        ),
+        bodyStyle: const TextStyle(
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+        ),
+        textFieldStyle: const TextStyle(
+          color: Colors.black,
+          //shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+        ),
+        buttonStyle: const TextStyle(
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+        ),
+        cardTheme: const CardTheme(
+          color: Colors.white,
+          elevation: 5,
+          margin: EdgeInsets.only(top: 15),
+          //shape: ContinuousRectangleBorder(
+          //    borderRadius: BorderRadius.circular(100.0)),
+        ),
+        inputTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.purple.withOpacity(.1),
+          contentPadding: EdgeInsets.zero,
+          errorStyle: const TextStyle(
+            backgroundColor: Colors.red,
+            color: Colors.white,
+          ),
+          labelStyle: const TextStyle(fontSize: 12),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.green.shade700, width: 4),
+            borderRadius: inputBorder,
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.green.shade400, width: 5),
+            borderRadius: inputBorder,
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+            borderRadius: inputBorder,
+          ),
+          focusedErrorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+            borderRadius: inputBorder,
+          ),
+          disabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 5),
+            borderRadius: inputBorder,
+          ),
+        ),
+        buttonTheme: const LoginButtonTheme(
+          splashColor: Colors.blue,
+          backgroundColor: Colors.green,
+          highlightColor: Colors.lightGreen,
+          elevation: 9.0,
+          highlightElevation: 6.0,
+          //shape: BeveledRectangleBorder(
+          //  borderRadius: BorderRadius.circular(10),
+          //),
+          // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+          // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+        ),
+      ),
+      
       onLogin: _loginUser,
       onSignup: _signUpUser,
       onRecoverPassword: _recoverPassword,
