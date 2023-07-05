efefc004b5598ebf720785157b50d80ba41f4fb1
MatteoRinaldi99 <matteo.rinaldi.2@studenti.unipd.it>
2023-07-05 23:31:57 +0200

    final ingredients list

--- lib/models/ingredientslist.dart
+++ lib/models/ingredientslist.dart
@@ -4,227 +4,371 @@ class IngredientsList{
 
   final List<Map> ingredientslist = [
     {'id': 1,
-    'name': 'Pasta',
-    'isSelected': false},
-
-    {'id': 2,
-    'name': 'Peeled tomatoes',
-    'isSelected': false},
-
-    {'id': 3,
-    'name': 'Extra virgin olive oil',
-    'isSelected': false},
-
-    {'id': 4,
-    'name': 'Garlic',
-    'isSelected': false},
-
-    {'id': 5,
-    'name': 'Basil',
-    'isSelected': false},
-
-    {'id': 6,
-    'name': 'Fine salt',
-    'isSelected': false},
-
-    {'id': 7,
-    'name': 'Coarse salt',
-    'isSelected': false},
-
-    {'id': 8,
-    'name': 'Egg whites',
-    'isSelected': false},
-
-    {'id': 9,
-    'name': 'Peanut butter',
-    'isSelected': false},
-
-    {'id': 10,
-    'name': 'Oat flakes',
-    'isSelected': false},
-   
-    {'id': 11,
-    'name': 'Hazelnut flour',
-    'isSelected': false},
-
-    {'id': 12,
-    'name': 'Acacia honey',
-    'isSelected': false},
-  
-    {'id': 13,
-    'name': 'Spaghetti',
-    'isSelected': false},
-    
-    {'id': 14,
-    'name': 'Tuna in oil',
-    'isSelected': false},
-
-    {'id': 15,
-    'name': 'Black pepper',
-    'isSelected': false},
-
-    {'id': 16,
-    'name': 'Golden onions',
-    'isSelected': false},
-
-    {'id': 17,
-    'name': 'Farfalle',
-    'isSelected': false},
-
-    {'id': 18,
-    'name': 'Zucchini',
-    'isSelected': false},
-
-    {'id': 19,
-    'name': 'Mini Pipe Rigate',
-    'isSelected': false},
-
-    {'id': 20,
-    'name': 'Auburn Tomatoes',
-    'isSelected': false},
-
-    {'id': 21,
-    'name': 'Fresh Spring Onion',
-    'isSelected': false},
-
-    {'id': 21,
-    'name': 'Pre-cooked Chickpeas',
-    'isSelected': false},
-
-    {'id': 22,
-    'name': 'Broccoli',
-    'isSelected': false},
-
-    {'id': 23,
-    'name': 'Water',
-    'isSelected': false},
-
-    {'id': 24,
-    'name': 'Black Pepper ',
-    'isSelected': false},
-
-    {'id': 25,
-    'name': 'Plaice Fillets',
-    'isSelected': false},
-
-    {'id': 26,
-    'name': 'Rosemary',
-    'isSelected': false},
-
-    {'id': 27,
-    'name': 'Sage',
-    'isSelected': false},
-
-    {'id': 28,
-    'name': 'Thyme',
-    'isSelected': false},
-
-    {'id': 29,
-    'name': 'Lemons',
-    'isSelected': false},
-
-    {'id': 30,
-    'name': 'Eggs',
-    'isSelected': false},
-
-    {'id': 31,
-    'name': 'White wine vinegar',
-    'isSelected': false},
-
-    {'id': 32,
-    'name': 'Whole milk',
-    'isSelected': false},
-
-    {'id': 33,
-    'name': 'Fuji apples',
-    'isSelected': false},
-
-    {'id': 34,
-    'name': 'Hazelnut kernels',
-    'isSelected': false},
-
-    {'id': 35,
-    'name': 'Cinnamon powder',
-    'isSelected': false},
-
-    {'id': 36,
-    'name': 'Salt',
-    'isSelected': false},
-
-    {'id': 37,
-    'name': 'Clams',
-    'isSelected': false},
-
-    {'id': 38,
-    'name': 'Parsley',
-    'isSelected': false},
-
-    {'id': 39,
-    'name': 'Chicken Breast',
-    'isSelected': false},
-
-    {'id': 40,
-    'name': 'Butter',
-    'isSelected': false},
-
-    {'id': 41,
     'name': '00 flour',
     'isSelected': false},
 
+    {'id': 2,
+    'name': 'Ananas',
+    'isSelected': false},
+
+    {'id': 3,
+    'name': 'Anchovies in oil',
+    'isSelected': false},
+
+    {'id': 4,
+    'name': 'Apples',
+    'isSelected': false},
+
+    {'id': 5,
+    'name': 'Artichokes',
+    'isSelected': false},
+
+    {'id': 6,
+    'name': 'Baby peas',
+    'isSelected': false},
+
+    {'id': 7,
+    'name': 'Banana',
+    'isSelected': false},
+
+    {'id': 8,
+    'name': 'Bread',
+    'isSelected': false},
+    
+    {'id': 9,
+    'name': 'Broccoli ',
+    'isSelected': false},
+
+    {'id': 10,
+    'name': 'Butter',
+    'isSelected': false},
+
+    {'id': 11,
+    'name': 'Capers',
+    'isSelected': false},
+
+    {'id': 12,
+    'name': 'Carrots',
+    'isSelected': false},
+
+    {'id': 13,
+    'name': 'Celery ',
+    'isSelected': false},
+
+    {'id': 14,
+    'name': 'Chicken ',
+    'isSelected': false},
+
+    {'id': 15,
+    'name': 'Chickpeas',
+    'isSelected': false},
+
+    {'id': 16,
+    'name': 'Clams',
+    'isSelected': false},
+
+    {'id': 17,
+    'name': 'Cleaned cuttlefish',
+    'isSelected': false},
+
+    {'id': 18,
+    'name': 'Cocoa',
+    'isSelected': false},
+
+    {'id': 19,
+    'name': 'Corn',
+    'isSelected': false},
+
+    {'id': 20,
+    'name': 'Dark chocolate',
+    'isSelected': false},
+
+    {'id': 21,
+    'name': 'Dried white beans',
+    'isSelected': false},
+
+    {'id': 22,
+    'name': 'Eggplant',
+    'isSelected': false},
+
+    {'id': 23,
+    'name': 'Eggs',
+    'isSelected': false},
+
+    {'id': 24,
+    'name': 'Fennel',
+    'isSelected': false},
+
+    {'id': 25,
+    'name': 'Fresh Spring Onion',
+    'isSelected': false},
+
+    {'id': 26,
+    'name': 'Fruit',
+    'isSelected': false},
+
+    {'id': 27,
+    'name': 'Grated cheese',
+    'isSelected': false},
+
+    {'id': 28,
+    'name': 'Green beans',
+    'isSelected': false},
+
+    {'id': 29,
+    'name': 'Green peppers',
+    'isSelected': false},
+
+    {'id': 30,
+    'name': 'Ham',
+    'isSelected': false},
+
+    {'id': 31,
+    'name': 'Hazelnut flour',
+    'isSelected': false},
+
+    {'id': 32,
+    'name': 'Hazelnut kernels',
+    'isSelected': false},
+
+    {'id': 33,
+    'name': 'Honey',
+    'isSelected': false},
+
+    {'id': 34,
+    'name': 'Hot paprika',
+    'isSelected': false},
+
+    {'id': 35,
+    'name': 'Jackdaws',
+    'isSelected': false},
+
+    {'id': 36,
+    'name': 'Leeks',
+    'isSelected': false},
+
+    {'id': 37,
+    'name': 'Lemons',
+    'isSelected': false},
+
+    {'id': 38,
+    'name': 'Marmalade',
+    'isSelected': false},
+
+    {'id': 39,
+    'name': 'Matured cheese',
+    'isSelected': false},
+
+    {'id': 40,
+    'name': 'Melon',
+    'isSelected': false},
+
+    {'id': 41,
+    'name': 'Milk',
+    'isSelected': false},
+
     {'id': 42,
-    'name': 'Salad',
+    'name': 'Mint',
     'isSelected': false},
 
     {'id': 43,
-    'name': 'Saffron',
+    'name': 'Monks beard (agretti)',
     'isSelected': false},
 
     {'id': 44,
-    'name': 'Rice',
+    'name': 'Mozzarella ',
     'isSelected': false},
 
     {'id': 45,
-    'name': 'Onion',
+    'name': 'Muesli',
     'isSelected': false},
 
     {'id': 46,
-    'name': 'Grana Padano cheese',
+    'name': 'Mussels',
     'isSelected': false},
 
     {'id': 47,
-    'name': 'White wine',
+    'name': 'Mustard',
     'isSelected': false},
 
     {'id': 48,
-    'name': 'Vegetable stock',
+    'name': 'Oat flakes',
     'isSelected': false},
 
     {'id': 49,
-    'name': 'Cherry tomatoes',
+    'name': 'Oatmeal',
     'isSelected': false},
 
     {'id': 50,
-    'name': 'Tomato puree',
+    'name': 'Octopus',
     'isSelected': false},
 
     {'id': 51,
-    'name': 'Salmon steaks',
+    'name': 'Orange',
     'isSelected': false},
 
     {'id': 52,
-    'name': 'Potatoes',
+    'name': 'Pasta',
     'isSelected': false},
 
     {'id': 53,
-    'name': 'Lemon peel',
+    'name': 'Peach pulp',
     'isSelected': false},
 
     {'id': 54,
-    'name': 'Lemon juice',
+    'name': 'Peanut butter',
     'isSelected': false},
 
     {'id': 55,
-    'name': 'Dry white wine',
+    'name': 'Pine nuts',
+    'isSelected': false},
+
+    {'id': 56,
+    'name': 'Pitted green olives',
+    'isSelected': false},
+
+    {'id': 57,
+    'name': 'Plaice Fillets',
+    'isSelected': false},
+
+    {'id': 58,
+    'name': 'Porcini mushrooms',
+    'isSelected': false},
+
+    {'id': 59,
+    'name': 'Potatoes',
+    'isSelected': false},
+
+    {'id': 60,
+    'name': 'Precooked couscous',
+    'isSelected': false},
+
+    {'id': 61,
+    'name': 'Red onions',
+    'isSelected': false},
+
+    {'id': 62,
+    'name': 'Red wine',
+    'isSelected': false},
+
+    {'id': 63,
+    'name': 'Rice',
+    'isSelected': false},
+
+    {'id': 64,
+    'name': 'Ricotta',
+    'isSelected': false},
+
+    {'id': 65,
+    'name': 'Saffron ',
+    'isSelected': false},
+
+    {'id': 66,
+    'name': 'Salad',
+    'isSelected': false},
+
+    {'id': 67,
+    'name': 'Salmon steaks',
+    'isSelected': false},
+
+    {'id': 68,
+    'name': 'Sausage',
+    'isSelected': false},
+
+    {'id': 69,
+    'name': 'Scampi',
+    'isSelected': false},
+
+    {'id': 70,
+    'name': 'Sea bream',
+    'isSelected': false},
+
+    {'id': 71,
+    'name': 'Seed oil',
+    'isSelected': false},
+
+    {'id': 72,
+    'name': 'Sesame seeds',
+    'isSelected': false},
+
+    {'id': 73,
+    'name': 'Shrimp',
+    'isSelected': false},
+
+    {'id': 74,
+    'name': 'Sliced swordfish',
+    'isSelected': false},
+
+    {'id': 75,
+    'name': 'Spaghetti',
+    'isSelected': false},
+
+    {'id': 76,
+    'name': 'Squid',
+    'isSelected': false},
+
+    {'id': 77,
+    'name': 'Strawberries',
+    'isSelected': false},
+
+    {'id': 78,
+    'name': 'Tomato puree',
+    'isSelected': false},
+
+    {'id': 79,
+    'name': 'Tomatoes',
+    'isSelected': false},
+
+    {'id': 80,
+    'name': 'Tuna in oil',
+    'isSelected': false},
+
+    {'id': 81,
+    'name': 'Turmeric powder',
+    'isSelected': false},
+
+    {'id': 82,
+    'name': 'Veal undercut',
+    'isSelected': false},
+
+    {'id': 83,
+    'name': 'Vegetable stock',
+    'isSelected': false},
+
+    {'id': 84,
+    'name': 'Walnuts',
+    'isSelected': false},
+
+    {'id': 85,
+    'name': 'White onions',
+    'isSelected': false},
+
+    {'id': 86,
+    'name': 'White wine',
+    'isSelected': false},
+
+    {'id': 87,
+    'name': 'White wine vinegar',
+    'isSelected': false},
+
+    {'id': 88,
+    'name': 'Whole grain cookies',
+    'isSelected': false},
+
+    {'id': 89,
+    'name': 'Yeast',
+    'isSelected': false},
+
+    {'id': 90,
+    'name': 'Yellow peppers',
+    'isSelected': false},
+
+    {'id': 91,
+    'name': 'Yogurt',
+    'isSelected': false},
+
+    {'id': 92,
+    'name': 'Zucchini',
     'isSelected': false},
 
   ];
